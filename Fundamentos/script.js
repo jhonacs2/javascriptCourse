05c661bf7fc109cf05481d471d476eb5521b8bab
Jhonatan Soto <jsoto@hansa.com.bo>
2021-03-25 14:02:44 -0400

    break y continue

---
+++ Fundamentos/script.js
@@ -0,0 +1,18 @@
+// Break and Continue
+// Break Rompe El Ciclo, break se salta la itereacion
+const numeros = [1,2,3,4,5,6,7,8,9,0];
+
+for (let i = 0; i <numeros.length ; i++){
+    if(i === 5 ){
+        break;
+    }
+    console.log(numeros[i]);
+}
+
+
+for (let i = 0; i <numeros.length ; i++){
+    if(i === 5 ){
+        continue;
+    }
+    console.log(numeros[i]);
+}
